74864f6345628f7563a99ac5a6c1d9d880988f16
mysterion186 <kowsikan@hotmail.fr>
2022-05-07 18:50:36 +0200

    affichage fichiers en cours d'upload + progression

--- interface/static/interface/script.js
+++ interface/static/interface/script.js
@@ -1,20 +1,40 @@
 const form = document.querySelector("form");
+form.reset();
 fileInput = form.querySelector(".file-input")
 progressArea = form.querySelector(".progress-area");
 uploadedArea = form.querySelector(".uploaded-area");
-
+console.log("Hello1");
 form.addEventListener("click", ()=>{
+    console.log("test eventlistener")
     fileInput.click();
 });
 
 fileInput.onchange = (e)=>{
+    console.log("test on change")
     let file = e.target.files[0];
+    console.log(e.target.files[0]/length);
     if (file){
         let fileName = file.name;
-        uploadFile(fileName);
+        console.log(fileName);
+        uploadFile();
+        console.log("upload appelé");
     }
 };
+// sub_button = document.querySelector(".submit_button");
+// sub_button.addEventListener("click",()=>{
+//     console.log("fichier envoyé");
+// })
 
-function uploadFile(name){
-    
+function uploadFile(){
+    var formData = new FormData(form);
+    var config = {
+        onUploadProgress: (progressEvent) => {
+            var filename = document.querySelector("#filename");
+            var file_percent = document.querySelector(".progress");
+            filename.innerHTML = formData.get("myfile")["name"];
+            file_percent.style.width = Math.round( (progressEvent.loaded * 100) / progressEvent.total ).toString()+"%";
+            console.log();
+        }
+    };
+    axios.post('',formData, config);
 }
--- interface/views.py
+++ interface/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
-
+from django.core.files.storage import FileSystemStorage
 # Create your views here.
 def index(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'interface/index.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
     return render(request,"interface/index.html")
