58c0afd5b488792cee0f5499f31b771b07f380a8
Mysterion186 <kowsikan@hotmail.fr>
2022-05-24 10:57:50 +0200

    ajout d'une croix pour supprimer les labels

--- interface/static/interface/signal.js
+++ interface/static/interface/signal.js
@@ -39,13 +39,8 @@ function makePlotly( traces,line_bottom,line_top ){
                     'width':3,
                 }
             })
-            create_label(layout,"xcoords","label_coordinate"); // fonction qui va ajouter le dernier label à la page html
-            var label_x = document.querySelectorAll(".label_coordinate");
-            label_x.forEach(elt => elt.addEventListener("click",() =>{delete_label(layout,elt.id);Plotly.redraw('myDiv');})); // on ajoute la possibilité de supprimer sur les labels en cliquant sur la valeur + maj du graph en enlevant ce label
-            // valeurs pour connaître la taille à donner aux labels (pour pas qu'ils soient trop petits ou trop grands) ici ils feront la "taille du signal"
-            // event pour changer la couleurs des barres verticales pour les identifier plus facilement
-            label_x.forEach(elt => elt.addEventListener("mouseover",()=>{change_color(elt.id,"blue",layout);}))
-            label_x.forEach(elt => elt.addEventListener("mouseleave",()=>{change_color(elt.id,"black",layout)}))
+            create_label(layout,"left","right"); // fonction qui va ajouter le dernier label à la page html
+            event_listener_label("label_coordinate",layout);
         }
         Plotly.redraw('myDiv');
     });
@@ -70,25 +65,37 @@ function makePlotly( traces,line_bottom,line_top ){
             })
             Plotly.redraw('myDiv'); // maj du graph
         }
-        create_label(layout,"xcoords","label_coordinate"); // fonction qui va ajouter le dernier label à la page html
-        // on recharge les labels à chaque instant pour être sûr d'être toujours à jour
-        var label_x = document.querySelectorAll(".label_coordinate");
-        label_x.forEach(elt => elt.addEventListener("click",() =>{delete_label(layout,elt.id);Plotly.redraw('myDiv');})); // on ajoute la possibilité de supprimer sur les labels en cliquant sur la valeur + maj du graph en enlevant ce label
-        // valeurs pour connaître la taille à donner aux labels (pour pas qu'ils soient trop petits ou trop grands) ici ils feront la "taille du signal"
-        // event pour changer la couleurs des barres verticales pour les identifier plus facilement
-        label_x.forEach(elt => elt.addEventListener("mouseover",()=>{change_color(elt.id,"blue",layout);}))
-        label_x.forEach(elt => elt.addEventListener("mouseleave",()=>{change_color(elt.id,"black",layout)}))
+        create_label(layout,"left","right"); // fonction qui va ajouter le dernier label à la page html
+        event_listener_label("label_coordinate",layout);
     });
     // fonction qui va à chaque fois que la souris est sûr le graphe va mettre à jour les valeurs des labels sur la page HTML
     plotDiv.addEventListener("mouseover", () =>{
-        update_coordinate(layout,"xcoords");
+        update_coordinate(layout);
     })
 };
 
+// fonction pour ajouter les différents events sur les coordonnées (suppression + changement de couleur de la ligne)
+function event_listener_label(class_name,layout){
+    var label_x = document.querySelectorAll("."+class_name);
+    // label_x.forEach(elt => elt.addEventListener("click",() =>{delete_label(layout,elt.id);Plotly.redraw('myDiv');})); // on ajoute la possibilité de supprimer sur les labels en cliquant sur la valeur + maj du graph en enlevant ce label
+    // valeurs pour connaître la taille à donner aux labels (pour pas qu'ils soient trop petits ou trop grands) ici ils feront la "taille du signal"
+    // event pour changer la couleurs des barres verticales pour les identifier plus facilement
+    label_x.forEach(elt => elt.addEventListener("mouseover",()=>{
+        delete_cross("cross"); // suppression des croix
+        change_color(elt.id,"blue",layout); // changement de couleur de la ligne verticale
+        const cross = document.getElementById("cross_"+elt.id); // on cherche à afficher la croix 
+        cross.innerHTML = "&#10060;"
+        cross.addEventListener("click",()=>{delete_label(layout,elt.id);Plotly.redraw('myDiv');})
+    }))
+    label_x.forEach(elt => elt.addEventListener("mouseleave",()=>{change_color(elt.id,"black",layout);}))
+}
 
 // fonction pour mettre à jour les coordonnées sur la page HTML
-function update_coordinate(layout,id_name) {
+function update_coordinate(layout) {
     var coordinates = document.querySelectorAll(".label_coordinate");
+    var crosses = document.querySelectorAll(".cross");
+    // console.log(crosses);
+    // il doit y avoir autant de coordonnées que de croix 
     for (var i = 0; i<layout["shapes"].length; i++){
         var coordinates = document.querySelectorAll(".label_coordinate");
         layout["shapes"][i]["x0"] = Math.round(layout["shapes"][i]["x0"]);
@@ -97,18 +104,23 @@ function update_coordinate(layout,id_name) {
             if (coordinates[i].id !== layout["shapes"][i]["x0"] ){
                 coordinates[i].innerHTML = 'Label au point : '+layout["shapes"][i]["x0"];
                 coordinates[i].id = layout["shapes"][i]["x0"];
+                crosses[i].id = "cross_"+layout["shapes"][i]["x0"];
             }
         }
     }
 }
 
 // fonction pour ajouter les coordonnées d'un nouveau label sur la page HTML
-function create_label(layout,id_name,class_name){
+function create_label(layout,id_name,id_name_2){
     var display = document.getElementById(id_name);
+    var display_r = document.getElementById(id_name_2);
     var result = "";
+    var result_r = "";
     if (layout["shapes"].length >0 ){
         result += '<div id='+layout["shapes"][layout["shapes"].length -1 ]["x0"]+' class="label_coordinate">Label au point : '+layout["shapes"][layout["shapes"].length -1 ]["x0"] +'</div>';
+        result_r = '<div id=cross_'+layout["shapes"][layout["shapes"].length -1 ]["x0"]+' class="cross"></div>';
         display.innerHTML +=result;
+        display_r.innerHTML+=result_r;
     }
     
 }
@@ -123,8 +135,17 @@ function delete_label(layout,elt_id){
     }
     var x_coord = document.getElementById(elt_id);
     x_coord.parentNode.removeChild(x_coord);
+    var coord_cross = document.getElementById("cross_"+elt_id);
+    coord_cross.parentNode.removeChild(coord_cross);
 }
 
+// fonction pour supprimer l'affichage des croix
+function delete_cross(class_name){
+    const crosses = document.querySelectorAll("."+class_name);
+    for (var i=0; i < crosses.length; i++){
+        crosses[i].innerHTML = "";
+    }
+}
 // fonction pour changer la couleur des barres verticales
 function change_color(elt_id, color,layout){
     for (var i=0; i < layout["shapes"].length; i++){
@@ -144,6 +165,7 @@ function change_color(elt_id, color,layout){
 // fonction dont le but est de supprimer les lables sur le côté 
 function reset_page(){
     set_validation_text();
+    delete_cross();
     const labels_zone = document.querySelectorAll(".label_coordinate");
     const choosed_file = document.querySelector(".choosed");
     if (choosed_file !== null) {
@@ -151,6 +173,10 @@ function reset_page(){
     }
     for (var i=0; i<labels_zone.length; i++) {
         labels_zone[i].parentNode.removeChild(labels_zone[i]);
+    }
+    const coord_cross = document.querySelectorAll(".cross");
+    for (var i = 0; i <coord_cross.length; i++){
+        coord_cross[i].parentNode.removeChild(coord_cross[i]);
     }
     Plotly.purge('myDiv');
 }
