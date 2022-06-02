e854daaae18b585170c5e9fbac84a6f62b233cc0
Mysterion186 <kowsikan@hotmail.fr>
2022-06-02 14:59:01 +0200

    en-tête des tests à réaliser

--- interface/tests/test_ui.py
+++ interface/tests/test_ui.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, tag
 from selenium import webdriver
+from bs4 import BeautifulSoup # pour scrapper la page et s'assurer que tous marche bien au niveau de l'affichage
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestUi(TestCase):
@@ -10,7 +11,11 @@ class TestUi(TestCase):
             desired_capabilities=DesiredCapabilities.CHROME
         )
     
-    def test_visit_site_with_chrome(self):
+    def test_init(self):
+        """
+            Test pour aller sur la page d'accueil
+            On envoie des fichiers au backend (en cliquant) et on s'assure que chaque fichier se trouve bien dans la liste des fichiers uploadé
+        """
         self.chrome.get('http://web:8000')
         js = """
             var identifier = document.querySelector(".log");
@@ -18,3 +23,33 @@ class TestUi(TestCase):
         """
         self.chrome.execute_script(js)
         self.assertIn(self.chrome.title, 'Ruptures Interface')
+    
+    def test_label_delete(self):
+        """
+            Test pour supprimer les labels 
+            Les fichiers json seront déjà là, on appuye sur un label pour afficher la croix, puis on clique dessus pour la supprimer
+            La suppression des labels se fera de manière aléatoire
+        """
+    
+    def test_label_hover(self):
+        """
+            Test pour voir si lorsqu'on passe sur une zone le label devient bel et bien bleu
+            utiliser bs4 pour check la couleur inscrit dans le tag
+        """
+
+    def test_label_add(self):
+        """
+            Test pour ajouter un label en cliquant sur la figure
+            (Test le plus dur, essayer de le faire sinon on laisse tomber)
+        """
+    
+    def test_label_move(self):
+        """
+            Test pour voir si on bouge un trait, est ce que le label se met à jour (à faire que si j'arrive à faire le test précédent)
+        """
+    
+
+    def test_prediction(self):
+        """
+            
+        """
