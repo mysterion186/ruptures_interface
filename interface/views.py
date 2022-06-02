caa595473013e09aef1f8339f53dfcb8eb17e8a8
Mysterion186 <kowsikan@hotmail.fr>
2022-06-02 13:13:15 +0200

    plus de zip dans le zip

--- interface/views.py
+++ interface/views.py
@@ -167,12 +167,13 @@ def download(request):
     # si on arrive jusqu'à la possibilité de télécharger un dossier c'est que tous les ruptures ont été prédites 
     folder_val = request.session["folder_val"]
     folder_path = str(MEDIA_ROOT)+"/"+str(folder_val)
-    output_path = folder_path+"/"+str(folder_val)
+    output_path = folder_path
     # conversion du dossier en un zip 
     shutil.make_archive(output_path, "zip",folder_path) # où on sauvegarde, le format, le dossier qu'on veut compresser
     with open(output_path+".zip","rb") as fh :
         response = HttpResponse(fh.read(),content_type='application/zip')
         response['Content-Disposition'] = 'attachment; filename='+folder_val+'.zip'
-        return response
+    os.remove(output_path+".zip")
+    return response
 
     
